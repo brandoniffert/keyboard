3956f0fb676dd4cddacb9d7d8ffea41db8dd2ebe
Brandon Iffert <brandoniffert@gmail.com>
2019-06-25 15:28:36 -0400

    Update keymap: reposition arrow keys

--- ergodox_ez/keymap.c
+++ ergodox_ez/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,              KC_Q,       KC_W,             KC_F,          KC_P,     KC_G,           KC_LBRACKET,
       LT(_NUMBERS,         KC_BSPACE), KC_A,             KC_R,          KC_S,     KC_T,           KC_D,
       MO(_SYMBOLS),        KC_Z,       KC_X,             KC_C,          KC_V,     KC_B,           KC_MINUS,
-      KC_UP,               KC_DOWN,    KC_TRNS,          KC_LALT,       KC_LSPO,
+      KC_TRNS,             KC_TRNS,    KC_TRNS,          KC_LALT,       KC_LSPO,
 
       // Left thumb
       KC_TRNS,             KC_TRNS,
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_RBRACKET,         KC_J,       KC_L,             KC_U,          KC_Y,     KC_SCOLON,      KC_QUOTE,
       KC_H,                KC_N,       KC_E,             KC_I,          KC_O,     MT(MOD_HYPR, KC_ENTER),
       KC_EQUAL,            KC_K,       KC_M,             KC_COMMA,      KC_DOT,   KC_SLASH,       MO(_MACROS),
-      KC_RSPC,             KC_LALT,    KC_LGUI,          KC_LEFT,       KC_RIGHT,
+      KC_RSPC,             KC_LEFT,    KC_DOWN,          KC_UP,         KC_RIGHT,
 
       // Right thumb
       KC_MEDIA_PLAY_PAUSE, KC_ESCAPE,
