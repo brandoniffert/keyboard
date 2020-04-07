896fe51e1e088d762d23f68641023e2d9d8437e4
Brandon Iffert <brandoniffert@gmail.com>
2020-04-07 12:00:06 -0400

    refactor: update keymap

--- ergodox_ez/keymap.c
+++ ergodox_ez/keymap.c
@@ -1,19 +1,13 @@
-#include "ergodox_ez.h"
-#include "debug.h"
-#include "action_layer.h"
+#include QMK_KEYBOARD_H
 #include "version.h"
 
-enum custom_layers {
-  _COLEMAK,
-  _MACROS,
-  _NUMBERS,
-  _SYMBOLS,
-  _QWERTY
-};
+#define BASE 0 // Default (Colemak)
+#define MACR 1 // Macros
+#define NUMS 2 // Numbers
+#define SYMB 3 // Symbols
 
 enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE,
-  EPRM,
+  EPRM = SAFE_RANGE,
   VRSN,
   RGB_SLD,
 
@@ -22,25 +16,25 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_COLEMAK] = LAYOUT_ergodox(
+  [BASE] = LAYOUT_ergodox(
       // Left hand
-      KC_GRAVE,            KC_1,       KC_2,             KC_3,          KC_4,     KC_5,           KC_PGUP,
-      KC_TAB,              KC_Q,       KC_W,             KC_F,          KC_P,     KC_G,           KC_LBRACKET,
-      LT(_NUMBERS,         KC_BSPACE), KC_A,             KC_R,          KC_S,     KC_T,           KC_D,
-      MO(_SYMBOLS),        KC_Z,       KC_X,             KC_C,          KC_V,     KC_B,           KC_MINUS,
-      KC_TRNS,             KC_TRNS,    KC_TRNS,          KC_LALT,       KC_LSPO,
+      KC_GRAVE,          KC_1,     KC_2,    KC_3,    KC_4,     KC_5,  KC_PGUP,
+      KC_TAB,            KC_Q,     KC_W,    KC_F,    KC_P,     KC_G,  KC_LBRC,
+      LT(NUMS, KC_BSPC), KC_A,     KC_R,    KC_S,    KC_T,     KC_D,
+      MO(SYMB),          KC_Z,     KC_X,    KC_C,    KC_V,     KC_B,  KC_MINS,
+      KC_TRNS,           KC_TRNS,  KC_TRNS, KC_LALT, KC_LSPO,
 
       // Left thumb
-      KC_TRNS,             KC_TRNS,
-      TG(_QWERTY),
-      MT(MOD_LGUI,         KC_ENTER),  CTL_T(KC_ESCAPE), LSFT(KC_LGUI),
+      KC_TRNS,           KC_TRNS,
+      KC_TRNS,
+      MT(MOD_LGUI, KC_ENTER),  CTL_T(KC_ESC), LSFT(KC_LGUI),
 
       // Right hand
-      KC_PGDOWN,           KC_6,       KC_7,             KC_8,          KC_9,     KC_0,           KC_BSLASH,
-      KC_RBRACKET,         KC_J,       KC_L,             KC_U,          KC_Y,     KC_SCOLON,      KC_QUOTE,
-      KC_H,                KC_N,       KC_E,             KC_I,          KC_O,     MO(_MACROS),
-      KC_EQUAL,            KC_K,       KC_M,             KC_COMMA,      KC_DOT,   KC_SLASH,       KC_HYPR,
-      KC_RSPC,             KC_LEFT,    KC_DOWN,          KC_UP,         KC_RIGHT,
+      KC_PGDN,   KC_6,       KC_7,             KC_8,          KC_9,     KC_0,           KC_BSLASH,
+      KC_RBRC,   KC_J,       KC_L,             KC_U,          KC_Y,     KC_SCOLON,      KC_QUOTE,
+      KC_H,      KC_N,       KC_E,             KC_I,          KC_O,     MO(MACR),
+      KC_EQUAL,  KC_K,       KC_M,             KC_COMMA,      KC_DOT,   KC_SLASH,       KC_MEH,
+      KC_RSPC,   KC_LEFT,    KC_DOWN,          KC_UP,         KC_RGHT,
 
       // Right thumb
       KC_MEDIA_PLAY_PAUSE, KC_ESCAPE,
@@ -48,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       LALT(KC_LGUI), KC_BSPACE,  KC_SPACE
   ),
 
-  [_MACROS] = LAYOUT_ergodox(
+  [MACR] = LAYOUT_ergodox(
       // Left hand
       KC_TRNS, KC_TRNS,        KC_TRNS,            KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_TRNS,
       KC_TRNS, KC_TRNS,        KC_TRNS,            KC_UP,        KC_TRNS,    KC_TRNS,        KC_TRNS,
@@ -74,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       RGB_HUD, KC_TRNS,        KC_TRNS
   ),
 
-  [_NUMBERS] = LAYOUT_ergodox(
+  [NUMS] = LAYOUT_ergodox(
       // Left hand
       KC_TRNS,        KC_F1,       KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_TRNS,
       KC_TRNS,        KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
@@ -100,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS,        KC_TRNS,     KC_TRNS
   ),
 
-  [_SYMBOLS] = LAYOUT_ergodox(
+  [SYMB] = LAYOUT_ergodox(
       // Left hand
       KC_TRNS, KC_TRNS,   KC_TRNS,      KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_TRNS,   KC_TRNS,      KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS,
@@ -124,144 +118,114 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, KC_TRNS,
       KC_TRNS,
       KC_TRNS, KC_TRNS,   KC_TRNS
-  ),
-
-  [_QWERTY] = LAYOUT_ergodox(
-      // Left hand
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
-      KC_TRNS, KC_Q,    KC_W,    KC_E,     KC_R,      KC_T,     KC_TRNS,
-      KC_TRNS, KC_A,    KC_S,    KC_D,     KC_F,      KC_G,
-      KC_TRNS, KC_Z,    KC_X,    KC_C,     KC_V,      KC_B,     KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-
-      // Left thumb
-      KC_TRNS, KC_TRNS,
-      KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS,
-
-      // Right hand
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,
-      KC_TRNS, KC_Y,    KC_U,    KC_I,     KC_O,      KC_P,     KC_TRNS,
-      KC_H,    KC_J,    KC_K,    KC_L,     KC_SCOLON, KC_TRNS,
-      KC_TRNS, KC_N,    KC_M,    KC_COMMA, KC_DOT,    KC_SLASH, KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-
-      // Right thumb
-      KC_TRNS, KC_TRNS,
-      KC_TRNS,
-      KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-  [1] = ACTION_LAYER_TAP_TOGGLE(1)
-};
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode) {
+      case EPRM:
+        eeconfig_init();
+        return false;
+      case VRSN:
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        return false;
+      #ifdef RGBLIGHT_ENABLE
+      case RGB_SLD:
+        rgblight_mode(1);
+        return false;
+      #endif
+      case MC_ARROW:
+        if (record->event.pressed) {
+          SEND_STRING("->");
+        }
+        return false;
+      case MC_HASH_ROCKET:
+        if (record->event.pressed) {
+          SEND_STRING("=>");
+        }
+        return false;
+    }
+  }
 
+  return true;
+}
+
+// Runs just one time when the keyboard initializes
 void matrix_init_user(void) {
 #ifdef RGBLIGHT_COLOR_LAYER_0
   rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
 #endif
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case EPRM:
-      if (record->event.pressed) {
-        eeconfig_init();
-      }
-      return false;
+// Runs whenever there is a layer state change
+layer_state_t layer_state_set_user(layer_state_t state) {
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+
+  uint8_t layer = get_highest_layer(state);
+  switch (layer) {
+    case 0:
+      #ifdef RGBLIGHT_COLOR_LAYER_0
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+      #else
+      #ifdef RGBLIGHT_ENABLE
+        rgblight_init();
+      #endif
+      #endif
       break;
-    case VRSN:
-      if (record->event.pressed) {
-        SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      }
-      return false;
+    case 1:
+      ergodox_right_led_1_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_1
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+      #endif
       break;
-    case RGB_SLD:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
+    case 2:
+      ergodox_right_led_2_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_2
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+      #endif
       break;
-    case MC_ARROW:
-      if (record->event.pressed) {
-        SEND_STRING("->");
-      }
-      return false;
+    case 3:
+      ergodox_right_led_3_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_3
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+      #endif
       break;
-    case MC_HASH_ROCKET:
-      if (record->event.pressed) {
-        SEND_STRING("=>");
-      }
-      return false;
+    case 4:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_4
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+      #endif
+      break;
+    case 5:
+      ergodox_right_led_1_on();
+      ergodox_right_led_3_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_5
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+      #endif
+      break;
+    case 6:
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_6
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+      #endif
+      break;
+    case 7:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_7
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_7);
+      #endif
+      break;
+    default:
       break;
   }
 
-  return true;
-}
-
-uint32_t layer_state_set_user(uint32_t state) {
-    uint8_t layer = biton32(state);
-
-    ergodox_board_led_off();
-    ergodox_right_led_1_off();
-    ergodox_right_led_2_off();
-    ergodox_right_led_3_off();
-    switch (layer) {
-      case 0:
-        #ifdef RGBLIGHT_COLOR_LAYER_0
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
-        #endif
-        break;
-      case 1:
-        ergodox_right_led_1_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_1
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
-        #endif
-        break;
-      case 2:
-        ergodox_right_led_2_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_2
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
-        #endif
-        break;
-      case 3:
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_3
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
-        #endif
-        break;
-      case 4:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_4
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
-        #endif
-        break;
-      case 5:
-        ergodox_right_led_1_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_5
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
-        #endif
-        break;
-      case 6:
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_6
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
-        #endif
-        break;
-      case 7:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_7
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
-        #endif
-        break;
-      default:
-        break;
-    }
-    return state;
+  return state;
 };
